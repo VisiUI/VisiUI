86ba241a06fe7a93db546c878332b46371653255
mineshopdev <mineshop47@gmail.com>
2024-08-18 10:14:13 +0200

    Removed the unused import of Image

--- visiui/src/app/page.tsx
+++ visiui/src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import React from "react";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
-      
-    </main>
+    <div>
+      Hi
+    </div>
   );
 }
