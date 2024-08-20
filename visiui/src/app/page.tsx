db5da71aadeb2566db2939eea8867abffeb766e6
mineshopdev <mineshop47@gmail.com>
2024-08-20 11:15:17 +0200

    Added Navbar to the page.tsx

--- visiui/src/app/page.tsx
+++ visiui/src/app/page.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import Endpoint from "@/data-components/Endpoint";
 import TextField from "@/ui-components/TextField";
 import Label from "@/ui-components/Label";
+import Navbar from "@/ui-components/navbar/navbar";
 
 const sampleValue = new Endpoint<string>({ value: "Edit me!" });
 
@@ -12,6 +13,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-center bg-slate-950">
       <Label value={sampleValue} />
       <TextField value={sampleValue} />
+      <Navbar />
     </main>
   );
 }
