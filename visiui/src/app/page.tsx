d96a58d14c404c7063f8c9f427cce7dfe018d5a9
Ayush Deb <ayushdeb848@gmail.com>
2024-08-21 17:10:02 +0530

    added footer

--- visiui/src/app/page.tsx
+++ visiui/src/app/page.tsx
@@ -5,6 +5,7 @@ import Endpoint from "@/data-components/Endpoint";
 import TextField from "@/ui-components/TextField";
 import Label from "@/ui-components/Label";
 import Nav from "@/ui-components/Navbar";
+import Footer from "@/ui-components/footer";
 import "../app/globals.css"
 
 const sampleValue = new Endpoint<string>({ value: "Edit me!" });
@@ -17,6 +18,7 @@ export default function Home() {
       <div className="flex flex-col items-center justify-center flex-grow text-gray-900 dark:text-white">
 
       </div>
+      <Footer/>
     </div>
   );
 }
