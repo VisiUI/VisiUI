0d7b514f71ad8582bc8ab767e0cc1e2a8a57bc1a
mineshopdev <mineshop47@gmail.com>
2024-08-24 13:16:02 +0200

    Switched to tailwind css

--- visiui/src/app/not-found.tsx
+++ visiui/src/app/not-found.tsx
@@ -1,26 +1,25 @@
 import Link from "next/link";
 import Button from "@/ui-components/Button";
-import "./not-found.css";
 
 export default function NotFound() {
   return (
-    <div className="notFoundContainer">
+    <div className="flex justify-center items-center text-center h-screen bg-gradient-to-r from-purple-500 to-blue-800">
       <div className="notFoundTextContainer">
-        <h1>404 - Page Not Found</h1>
-        <p>
+        <h1 className="text-5xl font-bold">404 - Page Not Found</h1>
+        <p className="mt-6 mx-auto max-w-max">
           Sorry, the page you&apos;re looking for doesn&apos;t exist. You might have
           entered an incorrect address, or the page may have been moved.
         </p>
         <Link href="/">
-          <Button
-            text="Homepage"
-            margin="40px"
-            backgroundColor="#fff"
-            padding="15px 20px"
-            borderRadius="40px"
-            border="none"
-            color="#000"
-          />
+            <Button
+              text="Homepage"
+              margin="40px"
+              backgroundColor="#fff"
+              padding="15px 20px"
+              borderRadius="40px"
+              border="none"
+              color="#000"
+            />
         </Link>
       </div>
     </div>
