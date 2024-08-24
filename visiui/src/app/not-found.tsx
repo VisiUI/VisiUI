d1012d70c12a5f6452b2ffa1f12abab5dc96f680
mineshopdev <mineshop47@gmail.com>
2024-08-24 13:21:32 +0200

    Ran prettier command

--- visiui/src/app/not-found.tsx
+++ visiui/src/app/not-found.tsx
@@ -7,19 +7,19 @@ export default function NotFound() {
       <div className="notFoundTextContainer">
         <h1 className="text-5xl font-bold">404 - Page Not Found</h1>
         <p className="mt-6 mx-auto max-w-max">
-          Sorry, the page you&apos;re looking for doesn&apos;t exist. You might have
-          entered an incorrect address, or the page may have been moved.
+          Sorry, the page you&apos;re looking for doesn&apos;t exist. You might
+          have entered an incorrect address, or the page may have been moved.
         </p>
         <Link href="/">
-            <Button
-              text="Homepage"
-              margin="40px"
-              backgroundColor="#fff"
-              padding="15px 20px"
-              borderRadius="40px"
-              border="none"
-              color="#000"
-            />
+          <Button
+            text="Homepage"
+            margin="40px"
+            backgroundColor="#fff"
+            padding="15px 20px"
+            borderRadius="40px"
+            border="none"
+            color="#000"
+          />
         </Link>
       </div>
     </div>
