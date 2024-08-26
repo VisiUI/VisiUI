c1d900f3dce940daae37239c849083567a8902e0
Ayush Deb <ayushdeb848@gmail.com>
2024-08-27 00:11:48 +0530

    made the footer

--- src/ui-components/footer.tsx
+++ src/ui-components/footer.tsx
@@ -1,135 +1,86 @@
-import React from "react";
-import Image from "next/image";
-import logo from "../../public/visiui8.png";
+import Image from 'next/image';
+import Link from 'next/link';
+import Logo from '../../public/visiui8.png';
 
 const Footer = () => {
   return (
-    <footer className="bg-black font-colvetica">
-      <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
-        <div className="md:flex md:justify-between">
-          <div className="mb-6 md:mb-0">
-            <a href="/" className="flex items-center">
-              <Image
-                src={logo}
-                alt="VisiUI"
-                width={50}
-                height={50}
-                className="h-16 w-auto"
-              />
-              <span className="self-center text-3xl font-semibold whitespace-nowrap text-white" style={{ letterSpacing: "1.5px" }}>
+    <footer className="bg-slate-50 py-8 border-t">
+      <div className="container mx-auto px-4">
+        <div className="flex flex-wrap justify-between items-start">
+          {/* Logo */}
+          <div className="w-full md:w-1/5 mb-4 md:mb-0">
+            <Image src={Logo} alt="Visiui Logo" width={100} height={50} />
+            <span className="self-center text-3xl font-semibold whitespace-nowrap text-black" style={{ letterSpacing: "0.5px", fontStyle:"oblique" }}>
                 VisiUI
               </span>
-            </a>
-            <span className="ml-4 mt-2 text-white">
-              VisiUI is a collection of reusable and
-              <span className="block ml-4 mt-1">
-                customizable React components
-              </span>
-            </span>
           </div>
-          <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
-            <div>
-              <h2 className="mb-6 text-sm font-semibold uppercase text-white" style={{ letterSpacing: "1.5px" }}>
-                Resources
-              </h2>
-              <ul className="text-white font-medium">
-                <li className="mb-4">
-                  <a href="/" className="hover:underline text-white">
-                    Documentation
-                  </a>
+
+          {/* Quick Links */}
+          <div className="w-full md:w-1/5 mb-4 md:mb-0">
+            <h3 className="font-bold mb-4 text-xl">Quick Links</h3>
+            <ul className="space-y-2">
+              {['Home', 'Services', 'Contact', 'Portfolio', 'Industries'].map((item) => (
+                <li key={item}>
+                  <Link href={`/${item.toLowerCase()}`} className="hover:text-purple-600">
+                    {item}
+                  </Link>
                 </li>
-                <li>
-                  <a href="/" className="hover:underline text-white">
-                    Blog
-                  </a>
-                </li>
-              </ul>
-            </div>
-            <div>
-              <h2 className="mb-6 text-sm font-semibold uppercase text-white" style={{ letterSpacing: "1.5px" }}>
-                Follow us
-              </h2>
-              <ul className="text-white font-medium">
-                <li className="mb-4">
-                  <a href="/" className="hover:underline text-white">
-                    Github
-                  </a>
-                </li>
-                <li>
-                  <a href="/" className="hover:underline text-white">
-                    Discord
-                  </a>
-                </li>
-              </ul>
-            </div>
-            <div>
-              <h2 className="mb-6 text-sm font-semibold uppercase text-white" style={{ letterSpacing: "1.5px" }}>
-                Legal
-              </h2>
-              <ul className="text-white font-medium">
-                <li className="mb-4">
-                  <a href="#" className="hover:underline text-white">
-                    Privacy Policy
-                  </a>
-                </li>
-                <li>
-                  <a href="#" className="hover:underline text-white">
-                    Terms & Conditions
-                  </a>
-                </li>
-              </ul>
-            </div>
+              ))}
+            </ul>
           </div>
+          
+          {/* Newsletter Subscription */}
+
+          
+<div className="w-full md:w-1/3 mb-4 md:mb-0 mr-16">
+  <h3 className="font-bold mb-4 text-xl">Subscribe To Our Newsletter</h3>
+  <div className="relative">
+    <input
+      type="email"
+      placeholder="Email Address"
+      className="w-full px-4 py-4 rounded-full border border-gray-950 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+    />
+    <button
+      type="submit"
+      className="absolute right-3 top-1/2 transform -translate-y-1/2 bg-purple-600 text-white px-5 py-3 rounded-full text-sm font-medium hover:bg-purple-700 transition duration-300"
+    >
+      Subscribe
+    </button>
+  </div>
+</div>
+
+          {/* Social Links */}
+          <div className="w-full md:w-1/6">
+  <h3 className="font-bold mb-4 text-xl">Follow Us</h3>
+  <div className="flex space-x-8">
+    <Link href="https://slack.com" target="_blank" rel="noopener noreferrer">
+      <div className="w-10 h-10 bg-gray-200 border border-black rounded-full flex items-center justify-center">
+        <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+          <path d="M5.042 15.165a2.528 2.528 0 0 1-2.52 2.523A2.528 2.528 0 0 1 0 15.165a2.527 2.527 0 0 1 2.522-2.52h2.52v2.52zM6.313 15.165a2.527 2.527 0 0 1 2.521-2.52 2.527 2.527 0 0 1 2.521 2.52v6.313A2.528 2.528 0 0 1 8.834 24a2.528 2.528 0 0 1-2.521-2.522v-6.313zM8.834 5.042a2.528 2.528 0 0 1-2.521-2.52A2.528 2.528 0 0 1 8.834 0a2.528 2.528 0 0 1 2.521 2.522v2.52H8.834zM8.834 6.313a2.528 2.528 0 0 1 2.521 2.521 2.528 2.528 0 0 1-2.521 2.521H2.522A2.528 2.528 0 0 1 0 8.834a2.528 2.528 0 0 1 2.522-2.521h6.312zM18.956 8.834a2.528 2.528 0 0 1 2.522-2.521A2.528 2.528 0 0 1 24 8.834a2.528 2.528 0 0 1-2.522 2.521h-2.522V8.834zM17.688 8.834a2.528 2.528 0 0 1-2.523 2.521 2.527 2.527 0 0 1-2.52-2.521V2.522A2.527 2.527 0 0 1 15.165 0a2.528 2.528 0 0 1 2.523 2.522v6.312zM15.165 18.956a2.528 2.528 0 0 1 2.523 2.522A2.528 2.528 0 0 1 15.165 24a2.527 2.527 0 0 1-2.52-2.522v-2.522h2.52zM15.165 17.688a2.527 2.527 0 0 1-2.52-2.523 2.526 2.526 0 0 1 2.52-2.52h6.313A2.527 2.527 0 0 1 24 15.165a2.528 2.528 0 0 1-2.522 2.523h-6.313z"/>
+        </svg>
+      </div>
+    </Link>
+    <Link href="https://github.com" target="_blank" rel="noopener noreferrer">
+      <div className="w-10 h-10 bg-gray-200 border border-black rounded-full flex items-center justify-center">
+        <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+          <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"/>
+        </svg>
+      </div>
+    </Link>
+    <Link href="https://discord.com" target="_blank" rel="noopener noreferrer">
+      <div className="w-10 h-10 bg-gray-200 border border-black rounded-full flex items-center justify-center">
+        <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+          <path d="M20.317 4.3698a19.7913 19.7913 0 00-4.8851-1.5152.0741.0741 0 00-.0785.0371c-.211.3753-.4447.8648-.6083 1.2495-1.8447-.2762-3.68-.2762-5.4868 0-.1636-.3933-.4058-.8742-.6177-1.2495a.077.077 0 00-.0785-.037 19.7363 19.7363 0 00-4.8852 1.515.0699.0699 0 00-.0321.0277C.5334 9.0458-.319 13.5799.0992 18.0578a.0824.0824 0 00.0312.0561c2.0528 1.5076 4.0413 2.4228 5.9929 3.0294a.0777.0777 0 00.0842-.0276c.4616-.6304.8731-1.2952 1.226-1.9942a.076.076 0 00-.0416-.1057c-.6528-.2476-1.2743-.5495-1.8722-.8923a.077.077 0 01-.0076-.1277c.1258-.0943.2517-.1923.3718-.2914a.0743.0743 0 01.0776-.0105c3.9278 1.7933 8.18 1.7933 12.0614 0a.0739.0739 0 01.0785.0095c.1202.099.246.1981.3728.2924a.077.077 0 01-.0066.1276 12.2986 12.2986 0 01-1.873.8914.0766.0766 0 00-.0407.1067c.3604.698.7719 1.3628 1.225 1.9932a.076.076 0 00.0842.0286c1.961-.6067 3.9495-1.5219 6.0023-3.0294a.077.077 0 00.0313-.0552c.5004-5.177-.8382-9.6739-3.5485-13.6604a.061.061 0 00-.0312-.0286zM8.02 15.3312c-1.1825 0-2.1569-1.0857-2.1569-2.419 0-1.3332.9555-2.4189 2.157-2.4189 1.2108 0 2.1757 1.0952 2.1568 2.419 0 1.3332-.9555 2.4189-2.1569 2.4189zm7.9748 0c-1.1825 0-2.1569-1.0857-2.1569-2.419 0-1.3332.9554-2.4189 2.1569-2.4189 1.2108 0 2.1757 1.0952 2.1568 2.419 0 1.3332-.946 2.4189-2.1568 2.4189Z"/>
+        </svg>
+      </div>
+    </Link>
+  </div>
+</div>
         </div>
-        <hr className="my-6 sm:mx-auto border-gray-700 lg:my-8" />
-        <div className="sm:flex sm:items-center sm:justify-between">
-          <span className="text-sm text-gray-400 sm:text-center text-white">
-            © 2024{" "}
-            <a href="/" className="hover:underline text-white">
-              VisiUI™
-            </a>
-            All Rights Reserved.
-          </span>
-          <div className="flex mt-4 sm:justify-center sm:mt-0">
-            <a
-              href="https://github.com/VisiUI/VisiUI"
-              className="text-gray-500 hover:text-white"
-            >
-              <svg
-                className="w-5 h-5"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 24 24"
-              >
-                <path d="M12 0C5.37 0 0 5.37 0 12c0 5.3 3.438 9.8 8.205 11.387.6.111.793-.26.793-.577v-2.234c-3.338.726-4.042-1.416-4.042-1.416-.546-1.387-1.333-1.758-1.333-1.758-1.09-.745.082-.73.082-.73 1.205.085 1.84 1.236 1.84 1.236 1.07 1.834 2.809 1.304 3.495.997.107-.775.418-1.305.762-1.605-2.665-.305-5.466-1.335-5.466-5.931 0-1.31.467-2.381 1.235-3.221-.124-.303-.535-1.527.118-3.18 0 0 1.008-.322 3.3 1.23.957-.267 1.984-.399 3.005-.403 1.02.004 2.047.136 3.006.403 2.291-1.552 3.297-1.23 3.297-1.23.654 1.653.243 2.877.119 3.18.77.84 1.234 1.911 1.234 3.221 0 4.61-2.803 5.623-5.474 5.921.43.372.814 1.102.814 2.222v3.293c0 .319.191.694.801.576C20.565 21.796 24 17.3 24 12c0-6.63-5.37-12-12-12z" />
-              </svg>
-              <span className="sr-only">GitHub Page</span>
-            </a>
-            <a
-              href="https://join.slack.com/t/visiui/shared_invite/zt-2oge4h1vn-J18RupLnaPKbmgF_AnhuSw"
-              className="text-gray-500 hover:text-white ms-5"
-            >
-              <svg
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-                stroke="#ffffff"
-                strokeWidth="0.00024000000000000003"
-                style={{ transform: "translateY(-4px)" }} // Adjust the value as needed
-              >
-                <path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M13 10C13 11.1046 13.8954 12 15 12C16.1046 12 17 11.1046 17 10V5C17 3.89543 16.1046 3 15 3C13.8954 3 13 3.89543 13 5V10ZM5 8C3.89543 8 3 8.89543 3 10C3 11.1046 3.89543 12 5 12H10C11.1046 12 12 11.1046 12 10C12 8.89543 11.1046 8 10 8H5ZM15 13C13.8954 13 13 13.8954 13 15C13 16.1046 13.8954 17 15 17H20C21.1046 17 22 16.1046 22 15C22 13.8954 21.1046 13 20 13H15ZM10 22C8.89543 22 8 21.1046 8 20L8 15C8 13.8954 8.89543 13 10 13C11.1046 13 12 13.8954 12 15V20C12 21.1046 11.1046 22 10 22ZM8 5C8 3.89543 8.89543 3 10 3C11.1046 3 12 3.89543 12 5V7H10C8.89543 7 8 6.10457 8 5ZM3 15C3 16.1046 3.89543 17 5 17C6.10457 17 7 16.1046 7 15V13H5C3.89543 13 3 13.8954 3 15ZM17 20C17 21.1046 16.1046 22 15 22C13.8954 22 13 21.1046 13 20V18H15C16.1046 18 17 18.8954 17 20ZM22 10C22 8.89543 21.1046 8 20 8C18.8954 8 18 8.89543 18 10V12H20C21.1046 12 22 11.1046 22 10Z"
-                  fill="#ffffff"
-                />
-              </svg>
-              <span className="sr-only">Slack Community</span>
-            </a>
-          </div>
+<hr className='my-6 sm:mx-auto border-gray-700 lg:my-8' />
+        {/* Copyright */}
+        <div className="text-center mt-8">
+          <p>&copy; Copyright 2024. All Rights Reserved</p>
         </div>
       </div>
     </footer>
