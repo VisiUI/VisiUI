3358c251bf0a45f17b37a65499a9fa700fb213ce
mineshopdev <mineshop47@gmail.com>
2024-08-29 16:20:01 +0200

    Resolved some sonarcloud issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,11 @@
 "use client";
 
 import React from "react";
-import { FloatingNav } from "@/ui-components/Navbar/FloatingNavbar";
 import { FloatingNavDemo } from "@/ui-components/Navbar/FloatingNavDemo";
-import About from "@/ui-components/About/About";
-import Footer from "../ui-components/Footer/footer";
 import Hero from "@/ui-components/Hero/Hero";
 import WhyChooseUs from "@/ui-components/WhyChooseUs/WhyChooseUs";
 import "../app/globals.css";
 import { Analytics } from "@vercel/analytics/react";
-
-//const sampleValue = new Endpoint<string>({ value: "Edit me!" });
 
 export default function Home() {
   return (
