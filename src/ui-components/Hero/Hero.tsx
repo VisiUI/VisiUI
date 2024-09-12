20583627747f0295023b43f197359ac903a1ae4b
angeldevildev <122638135+angeldevildev@users.noreply.github.com>
2024-09-12 14:53:53 +0200

    Added the docs link

--- src/ui-components/Hero/Hero.tsx
+++ src/ui-components/Hero/Hero.tsx
@@ -22,7 +22,7 @@ export default function Hero() {
       <div className="flex flex-row items-center gap-5">
         <div className="bg-white text-black rounded-md">
           <Link
-            href="/"
+            href="https://docs-visi-ui.vercel.app/"
             className={buttonVariants({ className: "px-6", size: "lg" })}
           >
             Docs
